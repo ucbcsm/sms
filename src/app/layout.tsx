5ab9c39755de6eff16f85d6b35aae87fe1bdb4c0
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-06-22 14:35:09 -0700

    fixx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import ClientProvider from "@/components/clientProvider";
 import { getServerSession } from "@/lib/api/auth";
+import { Suspense } from "react";
 
 const inter = Inter({
   // variable: "--font-inter",
@@ -20,10 +21,13 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession();
+  
   return (
     <html lang="en">
       <body className={`${inter.className}  antialiased`}>
+        <Suspense>
         <ClientProvider session={session}>{children}</ClientProvider>
+        </Suspense>
       </body>
     </html>
   );
