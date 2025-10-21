1c21116afc61bd3ad34923a2e503ddc3a453db06
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-10-21 09:04:29 +0200

    deploy cahnges

--- src/app/console/(institution)/forms/edit.tsx
+++ src/app/console/(institution)/forms/edit.tsx
@@ -66,11 +66,22 @@ export const EditInstitutionForm: React.FC<Props> = ({
           messageApi.success("Institution mise à jour avec succès.");
           setOpen(false);
         },
-        onError: () => {
-          messageApi.error(
-            "Une erreur est survenue lors de la mise à jour de l'institution."
-          );
-        },
+        onError: (error) => {
+          if ((error as any).status === 403) {
+            messageApi.error(
+              `Vous n'avez pas la permission d'effectuer cette action`
+            );
+          } else if ((error as any).status === 401) {
+            messageApi.error(
+              "Vous devez être connecté pour effectuer cette action."
+            );
+          } else {
+            messageApi.error(
+              (error as any)?.response?.data?.message ||
+                "Une erreur est survenue lors de la mise à jour de l'institution."
+            );
+          }
+        }
       }
     );
   };
