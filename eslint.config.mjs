7f564dc9058dc4ac93e459abf14ca7dd8f438bf6
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-11-08 01:04:46 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from 'eslint-config-next/typescript'
+// import nextTs from 'eslint-config-next/typescript'
 
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
+  // ...nextTs,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-sync-scripts": "off",
     },
   },
   // Override default ignores of eslint-config-next.
