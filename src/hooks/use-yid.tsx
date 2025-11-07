af00e12f2a912247338f9151d04e25207e497ae3
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-11-08 00:08:42 +0200

    fix error in useYid hook

--- src/hooks/use-yid.tsx
+++ src/hooks/use-yid.tsx
@@ -1,22 +1,28 @@
 import { getYearById } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const useYid = () => {
-  const [yid, setYidValue] = useState<number | undefined>();
+  const [yid, setYidValue] = useState<number | undefined>(() => {
+    const storedYid = localStorage.getItem("yid");
 
-     const {
-       data: year,
-       isPending,
-       isError,
-       error,
-       isLoading,
-       refetch
-     } = useQuery({
-       queryKey: ["year", yid],
-       queryFn: ({ queryKey }) => getYearById(Number(yid)),
-       enabled: !!yid,
-     });
+    if (storedYid) return parseInt(storedYid);
+
+    return undefined;
+  });
+
+  const {
+    data: year,
+    isPending,
+    isError,
+    error,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["year", yid],
+    queryFn: () => getYearById(Number(yid)),
+    enabled: !!yid,
+  });
 
   const setYid = (value: number) => {
     setYidValue(value);
@@ -27,12 +33,6 @@ export const useYid = () => {
     setYidValue(undefined);
     localStorage.removeItem("yid");
   };
-
-  useEffect(() => {
-    const storedYid = localStorage.getItem("yid");
-
-    if (storedYid) {setYid(parseInt(storedYid));}
-  }, []);
 
   return {
     yid,
