a937b2472d946ca5ccdc380dcc07b641d604615d
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-11-08 03:06:52 +0200

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,10 @@ import { isAuthenticated } from "./lib/api/auth";
 export default async function middleware(request: NextRequest) {
  
   const { pathname } = request.nextUrl;
-  if (pathname.startsWith("/_not-found") || pathname.startsWith("/404")) {
-    return NextResponse.next();
-  }
+
   const isOnLoginPage = pathname.startsWith("/auth/login");
 
-  // 🔥 Ne pas rediriger vers /config si on est déjà dessus
+  // Ne pas rediriger vers /config si on est déjà dessus
   const exists = await checkInstitutionExistence();
   if (!exists && !pathname.startsWith("/config")) {
     return NextResponse.redirect(new URL("/config", request.url));
@@ -20,19 +18,19 @@ export default async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  // 🔥 Ne pas rediriger vers /auth/login si on est déjà dessus
+  // Ne pas rediriger vers /auth/login si on est déjà dessus
   if ((!accessToken || !refreshToken) && !isOnLoginPage) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
   const userIsAuthenticated = await isAuthenticated(accessToken);
 
-  // 🔁 Si pas authentifié et pas déjà sur /auth/login → redirection vers /auth/login
+  //  Si pas authentifié et pas déjà sur /auth/login → redirection vers /auth/login
   if (!userIsAuthenticated && !isOnLoginPage) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
-  // ✅ Si authentifié mais il est SUR /auth/login → on le bloque là et on le redirige vers /
+  // Si authentifié mais il est SUR /auth/login → on le bloque là et on le redirige vers /
   if (userIsAuthenticated && isOnLoginPage) {
     return NextResponse.redirect(new URL("/", request.url));
   }
@@ -42,5 +40,5 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|.*\\..*|_not-found|404).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|.*\\..*).*)"],
 };
