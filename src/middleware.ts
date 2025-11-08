01f53ca549b73f594fbbf4ff48938050337f975d
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-11-08 02:09:50 +0200

    try

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,5 +39,5 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|.*\\..*).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|_not-found|.*\\..*).*)"],
 };
